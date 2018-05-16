98593fbe5b05ee3c66f3c6fd116c7315a0a7451a
Martin Velas <mato.velas@gmail.com>
2018-05-16 13:26:13 +0200

    Initial zero vertex in the graph

--- scripts/poses_to_graph.py
+++ scripts/poses_to_graph.py
@@ -2,8 +2,12 @@
 
 import sys
 from odometry_cnn_data import load_kitti_poses, get_delta_odometry
-from pose_graph import Edge3D
+from pose_graph import Edge3D, print_first_vertex
 
-odoms = get_delta_odometry(load_kitti_poses(sys.stdin))
+
+poses = load_kitti_poses(sys.stdin)
+print_first_vertex(poses[0].dof)
+
+odoms = get_delta_odometry(poses)
 for i in range(1, len(odoms)):
     print Edge3D(i-1, i, odoms[i].dof)
