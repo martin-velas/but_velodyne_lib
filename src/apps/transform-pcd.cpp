1a2d69560b6b7a9794ff8621cf46894ef3702125
Martin Velas <mato.velas@gmail.com>
2019-02-13 17:00:41 +0100

    Load TXT files as point cloud

--- src/apps/transform-pcd.cpp
+++ src/apps/transform-pcd.cpp
@@ -65,6 +65,26 @@ bool parse_arguments(int argc, char **argv,
     return true;
 }
 
+bool loadTxtCloud(const string &in_filename, pcl::PointCloud<pcl::PointXYZI> &cloud) {
+  ifstream infile(in_filename.c_str());
+  if(!infile.is_open()) {
+    return false;
+  }
+  string line;
+  while (std::getline(infile, line)) {
+    std::istringstream iss(line);
+    pcl::PointXYZI pt;
+    iss >> pt.x >> pt.y >> pt.z;
+    float intensity;
+    iss >> intensity;
+    if(!iss.eof()) {
+      pt.intensity = intensity;
+    }
+    cloud.push_back(pt);
+  }
+  return true;
+}
+
 int main (int argc, char** argv) {
 
   string in_filename, out_filename;
@@ -74,7 +94,20 @@ int main (int argc, char** argv) {
   }
 
   pcl::PointCloud<pcl::PointXYZI> cloud;
-  pcl::io::loadPCDFile(in_filename, cloud);
+
+  boost::filesystem::path in_path(in_filename);
+  string extension = in_path.extension().string();
+
+  if(extension == ".pcd") {
+    pcl::io::loadPCDFile(in_filename, cloud);
+  } else if (extension == ".txt") {
+    loadTxtCloud(in_filename, cloud);
+  } else {
+    cerr << "ERROR: unknown file extension " << extension << " for input " << in_filename << endl;
+    return EXIT_FAILURE;
+  }
+
+
   pcl::transformPointCloud(cloud, cloud, pose);
   cerr << pose.matrix() << endl;
   pcl::io::savePCDFileBinary(out_filename, cloud);
