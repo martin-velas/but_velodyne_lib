f436849873119f0dd706e30dafd5a6356e2e8e47
Martin Velas <mato.velas@gmail.com>
2019-04-23 10:41:14 +0200

    Reindexing the pose graph

---
+++ scripts/reorder_lines.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+import sys
+import argparse
+import json
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Reordering of lines")
+    parser.add_argument("--map_json", dest="map_json", type=str, required=True)
+    args = parser.parse_args()
+
+    lines = sys.stdin.readlines()
+
+    map_indices_str = json.load(open(args.map_json))
+    map_indices = {int(k): int(v) for k, v in map_indices_str.items()}
+    for i in sorted(map_indices.keys()):
+        print lines[map_indices[i]]
---
+++ scripts/reorder_pose_graph.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+
+import sys
+import argparse
+import json
+
+
+class Reindexer:
+    def __init__(self):
+        self.indices = {}
+        self.lastIndex = -1
+
+    def reindex(self, index):
+        if index in self.indices:
+            return self.indices[index]
+        else:
+            self.lastIndex += 1
+            self.indices[index] = self.lastIndex
+            return self.lastIndex
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Reordering of pose graph")
+    parser.add_argument("--out_graph", dest="out_graph", type=str, required=True)
+    parser.add_argument("--out_map_json", dest="out_map_json", type=str, required=True)
+    args = parser.parse_args()
+
+    indexer = Reindexer()
+    out_graph = open(args.out_graph, "w")
+
+    for line in sys.stdin.readlines():
+        tokens = line.split()
+        type = tokens[0]
+        if type.startswith("VERTEX"):
+            i1 = indexer.reindex(int(tokens[1]))
+            out_graph.write("%s %d %s\n" % (type, i1, " ".join(tokens[2:])))
+        elif type.startswith("EDGE"):
+            i1 = indexer.reindex(int(tokens[1]))
+            i2 = indexer.reindex(int(tokens[2]))
+            out_graph.write("%s %d %d %s\n" % (type, i1, i2, " ".join(tokens[3:])))
+        else:
+            sys.stderr.write("Unknown element type: %s\n" % (type,))
+            sys.exit(1)
+
+    json.dump(indexer.indices, open(args.out_map_json, "w"))
