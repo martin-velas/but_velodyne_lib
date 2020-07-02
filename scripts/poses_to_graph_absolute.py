623d1c0c06ae22f4a4b19491b7acba895dfdfa48
Martin Velas <ivelas@fit.vutbr.cz>
2020-07-02 10:55:01 +0200

    Absolute pose graph edges only for valid GNSS

--- scripts/poses_to_graph_absolute.py
+++ scripts/poses_to_graph_absolute.py
@@ -1,34 +1,43 @@
 #! /usr/bin/env python
 
-import sys
+import argparse
 from odometry_cnn_data import load_kitti_poses
 from pose_graph import Edge3D
 
 
-def load_inf_matrix_diagonals(filename):
+parser = argparse.ArgumentParser(description="Absolute pose graph edges from GNSS/INS poses.")
+parser.add_argument("--poses", dest="poses", type=str, required=True)
+parser.add_argument("--precisions", dest="precisions", type=str, required=True)
+parser.add_argument("--fixes", dest="fixes", type=str, required=True)
+parser.add_argument("--stdevs", dest="stdevs", type=str, required=True)
+parser.add_argument("--max_stdev", dest="max_stdev", type=float, required=True)
+args = parser.parse_args()
+
+
+def load_inf_matrix_diagonals(file):
     diagonals = []
-    for line in open(filename).readlines():
+    for line in file.readlines():
         diagonals.append(map(float, line.split()))
     return diagonals
 
 
-poses = load_kitti_poses(sys.stdin)
+ACCEPTED_FIXES = ["RTK_FIX", "RTK_FLOAT"]
 
-if len(sys.argv) > 1:
-    if len(sys.argv) == 2:
-        inf_matrix_diagonals = load_inf_matrix_diagonals(sys.argv[1])
-        assert len(inf_matrix_diagonals) == len(poses)
-    elif len(sys.argv) == 7:
-        diagonal = map(float, sys.argv[1:])
-        assert len(diagonal) == 6
-        inf_matrix_diagonals = [diagonal]*len(poses)
-    else:
-        sys.stderr.write("ERROR: expecting 6 values (diagonal of information matrix)\n")
-        sys.exit(1)
-else:
-    inf_matrix_diagonals = [None]*len(poses)
 
-print "VERTEX3 -1 0 0 0 0 0 0"
+with open(args.poses) as poses_file, open(args.precisions) as precisions_file, open(args.fixes) as fixes_file, open(args.stdevs) as stdevs_file:
 
-for (i, pose) in enumerate(poses):
-    print Edge3D(-1, i, pose.dof, inf_matrix_diagonals[i])
+    poses = load_kitti_poses(poses_file)
+
+    inf_matrix_diagonals = load_inf_matrix_diagonals(precisions_file)
+    assert len(poses) == len(inf_matrix_diagonals)
+
+    fixes = map(lambda l: l.strip(), fixes_file.readlines())
+    assert len(poses) == len(fixes)
+    stdevs = map(lambda l: max(map(float, l.split()[0:3])), stdevs_file.readlines())
+    assert len(poses) == len(stdevs)
+
+    print "VERTEX3 -1 0 0 0 0 0 0"
+
+    for i, (pose, fix, stdev, inf_matrix) in enumerate(zip(poses, fixes, stdevs, inf_matrix_diagonals)):
+        if (fix in ACCEPTED_FIXES) and (stdev < args.max_stdev):
+            print Edge3D(-1, i, pose.dof, inf_matrix)
