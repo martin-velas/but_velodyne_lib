3035163824fdb9778933a8aa9dcac4c2d202b882
Martin Velas <ivelas@fit.vutbr.cz>
2020-07-31 14:12:13 +0200

    Fix: Angle sync for missing cloud parts

--- src/apps/sync-velodyne-clouds-by-angle.cpp
+++ src/apps/sync-velodyne-clouds-by-angle.cpp
@@ -43,7 +43,7 @@ namespace po = boost::program_options;
 const float FPS = 10.0;
 
 bool parse_arguments(int argc, char **argv, float &start_angle, int &velodyne_idx,
-                     string &output_dir, vector<string> &clouds_to_process) {
+                     string &output_dir, vector<string> &clouds_to_process, float &fps) {
 
   po::options_description desc("Synchronization of Velodynes in cloud meassurements\n"
                                "======================================\n"
@@ -53,6 +53,7 @@ bool parse_arguments(int argc, char **argv, float &start_angle, int &velodyne_id
     ("start_angle,s", po::value<float>(&start_angle)->required(), "Angle [deg] where the frame should start.")
     ("velodyne_idx,i", po::value<int>(&velodyne_idx)->required(), "Index of the Velodyne sensor.")
     ("output_dir,o", po::value<string>(&output_dir)->required(), "Output dir for PCD files.")
+    ("fps,f", po::value<float>(&fps)->default_value(10.0), "Velodyne FPS.")
     ;
   po::variables_map vm;
   po::parsed_options parsed = po::parse_command_line(argc, argv, desc);
@@ -100,31 +101,47 @@ ostream& operator << (ostream& stream, const AngleAndPhase &ap) {
   stream << ap.phase << " " << ap.angle << " " << ap.source_cloud_idx;
 }
 
+class Interval {
+public:
+    Interval(const float from_, const float to_) : from(from_), to(to_) {
+    }
 
-void get_borders(const vector<string> &cloud_filenames, const float start_angle,
-                 vector<AngleAndPhase> &borders) {
-  vector<AngleAndPhase> angles_phases;
-  const float EPS = 1.0; // [deg]
+    float from, to;
+};
 
-  for(int i = 0; i < cloud_filenames.size(); i++) {
-    VelodynePointCloud cloud;
-    VelodynePointCloud::fromFile(cloud_filenames[i], cloud);
-    for(VelodynePointCloud::const_iterator pt = cloud.begin(); pt < cloud.end(); pt++) {
-      const float angle = horizontalAngle(*pt);
-      if(start_angle-EPS < angle && angle < start_angle+EPS) {
-        angles_phases.push_back(AngleAndPhase(angle, pt->phase, i));
-      }
+size_t get_cloud_where_border_fits(const vector<Interval> &raw_borders_sorted, const float border) {
+  for(size_t i = 0; i < raw_borders_sorted.size(); i++) {
+    if(border < raw_borders_sorted[i].to) {
+      return i;
     }
   }
-  sort(angles_phases.begin(), angles_phases.end());
+}
 
-  float previous_angle = angles_phases.front().angle;
-  for(vector<AngleAndPhase>::const_iterator ap = angles_phases.begin(); ap < angles_phases.end(); ap++) {
-    if(previous_angle < start_angle && start_angle <= ap->angle &&
-       (borders.empty() || ((borders.back().phase + (1.0/FPS)/2.0) < ap->phase))) {
-      borders.push_back(*ap);
+void get_borders(const vector<string> &cloud_filenames, const float fps, const float start_angle,
+                 vector<AngleAndPhase> &borders) {
+  const float frame_duration = 1.0f / fps;
+
+  vector<Interval> raw_frames_borders;
+  float start_time;
+  for(size_t i = 0; i < cloud_filenames.size(); i++) {
+    VelodynePointCloud cloud;
+    VelodynePointCloud::fromFile(cloud_filenames[i], cloud);
+    raw_frames_borders.push_back(Interval(cloud.front().phase, cloud.back().phase));
+
+    if(i == 0) {
+      float angle_offset = start_angle - horizontalAngle(cloud.front());
+      if(angle_offset < 0) {
+        angle_offset += 360.0;
+      }
+      float phase_offset = angle_offset / 360.0;
+      start_time = cloud.front().phase + frame_duration * phase_offset;
     }
-    previous_angle = ap->angle;
+  }
+
+  float border;
+  for(int i = 0; (border = start_time + i / fps) < raw_frames_borders.back().to; i++) {
+    int cloud_idx = get_cloud_where_border_fits(raw_frames_borders, border);
+    borders.push_back(AngleAndPhase(start_angle, border, cloud_idx));
   }
 }
 
@@ -142,7 +159,6 @@ struct PhaseComparator {
 
 void sync_by_angle(const AngleAndPhase &beginning, const AngleAndPhase &end,
                    const VelodynePointCloud &input, VelodynePointCloud &frame_synced) {
-  //static Visualizer3D vis;
 
   VelodynePointCloud input_sorted;
   input_sorted += input;
@@ -150,34 +166,23 @@ void sync_by_angle(const AngleAndPhase &beginning, const AngleAndPhase &end,
   sort(input_sorted.begin(), input_sorted.end(), phase_cmp);
   bool recording = false;
   int i = 0;
-  // cerr << "Beginning: [" << beginning << "], end: [" << end << "]" << endl;
+
   for(VelodynePointCloud::iterator pt = input_sorted.begin(); pt < input_sorted.end(); pt++, i++) {
     float angle = horizontalAngle(*pt);
-    // cerr << "iff [" << beginning << "] <= " << pt->phase << ", " << angle << " < [" << end << "]" << endl;
+
     if(!recording && pt->phase >= beginning.phase && angle >= beginning.angle) {
       recording = true;
     }
+
     if(pt->phase >= end.phase && angle >= end.angle) {
       break;
     }
+
     if(recording) {
-      // float original_phase = pt->phase;
       pt->phase = (pt->phase - beginning.phase) / (end.phase - beginning.phase);
       frame_synced.push_back(*pt);
-      /* if(i % 231 == 0) {
-        char text[32];
-        sprintf(text, "%.2f/%.2f", original_phase, angle);
-        uchar r, g, b;
-        Visualizer3D::colorizeIntensity(pt->phase, r, g, b);
-        vis.getViewer()->addText3D(text, *pt, 0.05, r/255.0, g/255.0, b/255.0, text);
-      }*/
     }
   }
-
-  /* vis.keepOnlyClouds(0)
-    .addColorPointCloud(Visualizer3D::colorizeCloudByPhase(frame_synced))
-    .show();
-  vis.getViewer()->removeAllShapes();*/
 }
 
 int main(int argc, char** argv) {
@@ -186,34 +191,29 @@ int main(int argc, char** argv) {
   vector<string> cloud_filenames;
   string output_dir;
   int velodyne_idx;
+  float fps;
 
-  if(!parse_arguments(argc, argv, start_angle, velodyne_idx, output_dir, cloud_filenames)) {
+  if(!parse_arguments(argc, argv, start_angle, velodyne_idx, output_dir, cloud_filenames, fps)) {
     return EXIT_FAILURE;
   }
 
   vector<AngleAndPhase> borders;
-  get_borders(cloud_filenames, start_angle, borders);
-  cerr << "Borders found: " << borders.size() << endl;
+  get_borders(cloud_filenames, fps, start_angle, borders);
 
   stringstream ss_borders_fn;
   ss_borders_fn << output_dir << "/frame-beginnings." << velodyne_idx << ".txt";
   ofstream borders_fd(ss_borders_fn.str().c_str());
+  borders_fd << fixed;
 
   for(int i = 0; i+1 < borders.size(); i++) {
     const AngleAndPhase &beginning = borders[i];
     const AngleAndPhase &end = borders[i+1];
 
-    if(end.source_cloud_idx - beginning.source_cloud_idx > 1) {
-      cerr << "ERROR! Frame skipped between the borders [" << beginning << "] and [" << end << "]" << endl;
-      exit(1);
-    }
-
     VelodynePointCloud raw_cloud;
-    VelodynePointCloud::fromFile(cloud_filenames[beginning.source_cloud_idx], raw_cloud);
-    if(beginning.source_cloud_idx != end.source_cloud_idx) {
-      VelodynePointCloud second_half;
-      VelodynePointCloud::fromFile(cloud_filenames[end.source_cloud_idx], second_half);
-      raw_cloud += second_half;
+    for(int raw_i = beginning.source_cloud_idx; raw_i <= end.source_cloud_idx; raw_i++) {
+      VelodynePointCloud raw_part;
+      VelodynePointCloud::fromFile(cloud_filenames[raw_i], raw_part);
+      raw_cloud += raw_part;
     }
 
     VelodynePointCloud frame_synced;
